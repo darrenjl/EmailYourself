18bac9616b1a93a50b113cf628c6e34b062c0885
development <development@guido.oz.moneyswitch.net>
2014-04-17 12:35:19 +1000

    Removing old files from initial attempt

--- first_attempt/content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  alert("test");
-  chrome.tabs.getCurrent(function(tab){
-      alert(tab.url);
-  });
-});
--- first_attempt/devtools.js
+++
@@ -1,8 +0,0 @@
-chrome.devtools.panels.create(
-    "EmailYourslef", 
-    "icon.png", 
-    "index.html",
-    function() {
- 
-    }
-);
