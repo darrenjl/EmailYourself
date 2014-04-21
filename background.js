e3a07b9dc6d4e7c942cc3111befe14fbedfe4017
Darren Lyons <d.lyons88@gmail.com>
2014-04-21 14:00:28 +1000

    Revert "Attempting to add email functionality to background, sends successfully but can't save email anymore."
    This reverts commit 3bce4cfb95e8ac555e601752676cb1c577382820.

--- background.js
+++ background.js
@@ -9,61 +9,11 @@ function getPageInfo(callback) {
     chrome.tabs.executeScript(null, { file: 'content_script.js' }); 
 }; 
 
-function sendEmailIfAvailable(callback) {
-    callbacks.push(callback); 
-    exists = emailExists();
-    if (exists) {        
-        alert('email does exist'); 
-        chrome.tabs.executeScript(null, { file: 'content_script.js' });       
-    } else {
-        console.error('no email provided');
-        alert('no email provided');
-    }
-}
-
 // Perform the callback when a request is received from the content script
 chrome.extension.onMessage.addListener(function(request)  { 
     // Get the first callback in the callbacks array
     // and remove it from the array
-    alert('content_script result: ' + request);    
-    sendEmail(decodeURIComponent(localStorage.email), request.title, encodeURIComponent(request.url));     
+    var callback = callbacks.shift();
+    // Call the callback function
+    callback(request); 
 }); 
-
-function emailExists(){
-    alert('emailExists()'); 
-    email = localStorage.email;
-
-    if(email == undefined || email == "") {
-        return false;
-    }
-    return true;    
-};
-
-function saveEmail() {
-    event.preventDefault();
-
-    var email = encodeURIComponent(document.getElementById('email').value);    
-    localStorage.email = email;
-    chrome.extension.getBackgroundPage().getPageInfo(onPageInfo); 
-};
-
-function sendEmail(email, subject, body) {
-    alert('sendEmail')
-    $.ajax({ 
-             type: "GET",
-             dataType: "json",
-             url: "https://script.google.com/macros/s/AKfycbwpNFmh9INcnlaLdW5WZxKVaWDaFMljAI6Mo4EJ_qYdW6XbRC2F/exec?email=" + email + "&subject=" + subject + "&body=" + body,
-             success: function(data){        
-                var callback = callbacks.shift();
-                callback(request); 
-                console.log(data);
-                alert(data);
-             },
-             error: function(XMLHttpRequest, textStatus, errorThrown) {
-                console.error(textStatus);
-                console.error(errorThrown);
-                alert(textStatus);
-                alert(errorThrown);
-            }
-         });
-};
