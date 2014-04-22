aed1b28e73036d8c474791bc0460d7443587f4c1
Darren Lyons <d.lyons88@gmail.com>
2014-04-22 22:31:25 +1000

    Adding basic activity.

---
+++ android/EmailYourself/src/me/darrenlyons/emailyourself/activities/MainActivity.java
@@ -0,0 +1,12 @@
+package me.darrenlyons.emailyourself.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import me.darrenlyons.emailyourself.R;
+
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
